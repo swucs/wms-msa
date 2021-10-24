33d8ba5f937bb224eaa792421e09069e2ff97c78
Hanna Park <swucs7@gmail.com>
2021-10-25 00:15:03 +0900

    merchantNumber => businessNumber 수정

--- customer-service/src/main/java/com/sycoldstorage/customerservice/dto/SearchCustomerResponse.java
+++ customer-service/src/main/java/com/sycoldstorage/customerservice/dto/SearchCustomerResponse.java
@@ -11,16 +11,11 @@ public class SearchCustomerResponse {
 
     private int id;
     private String name;
-    private String merchantNumber;
+    private String businessNumber;
     private String representativeName;
     private String BusinessConditions;
     private String typeOfBusiness;
     private String phoneNumber;
     private String faxNumber;
     private boolean use;
-
-    private String getUseYn() {
-        return use ? "사용" : "미사용";
-    }
-
 }

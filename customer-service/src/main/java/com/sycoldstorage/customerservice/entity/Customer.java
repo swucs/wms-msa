62e91a2dd21207a5df4af16baa694c780849ffb9
Hanna Park <swucs7@gmail.com>
2021-12-30 14:15:50 +0900

    customer null옵션 수정

--- customer-service/src/main/java/com/sycoldstorage/customerservice/entity/Customer.java
+++ customer-service/src/main/java/com/sycoldstorage/customerservice/entity/Customer.java
@@ -2,6 +2,7 @@ package com.sycoldstorage.customerservice.entity;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -24,14 +25,30 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String businessNumber;
+
+    @Column(nullable = false)
     private String representativeName;
+
     private String businessConditions;
+
     private String typeOfBusiness;
+
+    @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false)
     private String phoneNumber;
+
     private String faxNumber;
+
+    @Column(nullable = false)
     private boolean use;
+
+    @Column(nullable = false)
     private boolean deleted;
 }
